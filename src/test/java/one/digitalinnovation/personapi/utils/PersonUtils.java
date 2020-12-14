df07e2673a7bdc21675d3e7dc97fe3357af4afee
Felipe Dantas <felipeacdantas@gmail.com>
2020-12-14 05:37:00 -0300

    "end"

--- src/test/java/one/digitalinnovation/personapi/utils/PersonUtils.java
+++ src/test/java/one/digitalinnovation/personapi/utils/PersonUtils.java
@@ -8,18 +8,18 @@ import java.util.Collections;
 
 public class PersonUtils {
 
-    private static final String FIRST_NAME = "Rodrigo";
-    private static final String LAST_NAME = "Peleias";
-    private static final String CPF_NUMBER = "369.333.878-79";
+    private static final String FIRST_NAME = "Felipe";
+    private static final String LAST_NAME = "Dantas";
+    private static final String CPF_NUMBER = "682.806.070-99";
     private static final long PERSON_ID = 1L;
-    public static final LocalDate BIRTH_DATE = LocalDate.of(2010, 10, 1);
+    public static final LocalDate BIRTH_DATE = LocalDate.of(2000, 01, 1);
 
     public static PersonDTO createFakeDTO() {
         return PersonDTO.builder()
                 .firstName(FIRST_NAME)
                 .lastName(LAST_NAME)
                 .cpf(CPF_NUMBER)
-                .birthDate("04-04-2010")
+                .birthDate("01-01-2010")
                 .phones(Collections.singletonList(PhoneUtils.createFakeDTO()))
                 .build();
     }
